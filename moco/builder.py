2471f59e4373aeec85a8b46aefb7938a96786135
Tian Pan <pantianauraro@foxmail.com>
2021-08-18 13:48:09 +0800

    Update builder.py

--- moco/builder.py
+++ moco/builder.py
@@ -2,8 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Generator_Mask(nn.Module):
-    """ generator for the mask:topk should be adaptive for the training process"""
+    '''
+    generator for the mask:
+    topk can be adaptive for the training process
+    '''
+
     def __init__(self, C_in, embedding_dim, hidden_dim, output_dim,
                  n_layers=3, kernel_size=3, padding=1, use_bidirectional=True, use_dropout=False):
         super().__init__()
@@ -11,7 +16,8 @@ class Generator_Mask(nn.Module):
             nn.Conv2d(C_in, C_in, kernel_size, stride=1, padding=padding, bias=False, dilation=2),
             nn.BatchNorm2d(C_in, eps=1e-3, affine=True),
             nn.ReLU(inplace=False),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=padding))
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=padding)
+        )
         in_channel = C_in
         depth = in_channel
         self.n_layers = n_layers
@@ -30,6 +36,7 @@ class Generator_Mask(nn.Module):
         self.atrous_block6 = nn.Conv2d(in_channel, depth, 3, 1, padding=6, dilation=6)
         self.atrous_block12 = nn.Conv2d(in_channel, depth, 3, 1, padding=12, dilation=12)
         self.atrous_block18 = nn.Conv2d(in_channel, depth, 3, 1, padding=18, dilation=18)
+
         self.conv_1x1_output = nn.Conv2d(depth * 5, depth, 1, 1)
         self.rnn = nn.LSTM(embedding_dim, hidden_dim // 2,
                            bidirectional=use_bidirectional,
@@ -37,9 +44,29 @@ class Generator_Mask(nn.Module):
         self.fc = nn.Sequential(nn.Linear(hidden_dim, output_dim), nn.Softmax())
         self.dropout = nn.Dropout(0.5 if use_dropout else 0.)
 
+    def attention_net(self, lstm_output, final_state):
+        lstm_output = lstm_output.permute(1, 0, 2)
+        hidden = final_state.squeeze(0)
+        attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
+        soft_attn_weights = F.softmax(attn_weights, 1)
+        new_hidden_state = torch.bmm(lstm_output.transpose(1, 2),
+                                     soft_attn_weights.unsqueeze(2)).squeeze(2)
+
+        return new_hidden_state
+
+    def attention(self, lstm_output, final_state):
+        lstm_output = lstm_output.permute(1, 0, 2)
+        merged_state = torch.cat([s for s in final_state], 1)
+        merged_state = merged_state.squeeze(0).unsqueeze(2)
+        weights = torch.bmm(lstm_output, merged_state)
+        weights = F.softmax(weights.squeeze(2)).unsqueeze(2)
+
+        return torch.bmm(torch.transpose(lstm_output, 1, 2), weights).squeeze(2)
+
     def forward(self, x):
         for i in range(self.n_layers):
             x = self.Conv_BN(x)
+        # ASPP
         size = x.shape[2:]
         b, c = x.shape[0], x.shape[1]
         image_features = self.mean(x)
@@ -48,6 +75,7 @@ class Generator_Mask(nn.Module):
         atrous_block1 = self.atrous_block1(x)
         atrous_block6 = self.atrous_block6(x)
         atrous_block12 = self.atrous_block12(x)
+
         atrous_block18 = self.atrous_block18(x)
         net = self.conv_1x1_output(torch.cat([image_features, atrous_block1, atrous_block6,
                                               atrous_block12, atrous_block18], dim=1))
@@ -61,26 +89,41 @@ class Generator_Mask(nn.Module):
 
         return self.fc(output[:, -1, :])
 
-class MaskGenerator(nn.Module):
-    def __init__(self):
-        super(MaskGenerator, self).__init__()
-        self.mask_generator = Generator_Mask(C_in=32 * 3, embedding_dim=169, hidden_dim=128, output_dim=32)
-
-    def forward(self, im_q):
-        # compute query features
-        b, c, n, w, h = im_q.shape
-        reshape = im_q.view(b, -1, w, h)
-        list_out = self.mask_generator(reshape)
+class VideoTopk(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, im_q, list_out):
+        b, c, n, h, w = im_q.shape
         values, indices = list_out.topk(8, dim=1, largest=False, sorted=False)
-        im_q_fake = im_q.clone()
+        im_q_fake = im_q.clone().detach()
         for i in range(b):
             for j in indices[i]:
                 im_q_fake[i, :, j, :, :] = torch.mean(im_q_fake[i, :, j, :, :])
         return im_q_fake
 
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_im = None
+        grad_list = torch.sum(grad_output, dim=(1, 3, 4))
+        return grad_im, grad_list
+
+class MaskGenerator(nn.Module):
+    def __init__(self):
+        super(MaskGenerator, self).__init__()
+        self.mask_generator = Generator_Mask(C_in=32 * 3, embedding_dim=169, hidden_dim=128, output_dim=32)
+        self.video_topk = VideoTopk()
+
+    def forward(self, im_q):
+        # compute query features
+        b, c, n, h, w = im_q.shape
+        reshape = im_q.view(b, -1, h, w)
+        list_out = self.mask_generator(reshape)
+        im_q_fake = self.video_topk.apply(im_q,list_out)
+        return im_q_fake
 
 class MoCo(nn.Module):
-
+    """
+    Build a MoCo model with: a query encoder, a key encoder, and a queue
+    """
     def __init__(self, base_encoder, dim=128, K=65536, m=0.999, T=0.07, mlp=False):
         """
         dim: feature dimension (default: 128)
@@ -189,7 +232,7 @@ class MoCo(nn.Module):
 
         return x_gather[idx_this]
 
-    def forward(self, im_q_fake, im_q, im_k, loss_gan):
+    def forward(self, im_q_fake, im_q, im_k, t):
         """
         Input:
             im_q: a batch of query images
@@ -197,6 +240,7 @@ class MoCo(nn.Module):
         Output:
             logits, targets
         """
+        torch.autograd.set_detect_anomaly(True)
         q_real = self.encoder_q(im_q)
         q_real = nn.functional.normalize(q_real, dim=1)
         q_fake = self.encoder_q(im_q_fake)  # queries: NxC
@@ -220,7 +264,7 @@ class MoCo(nn.Module):
         # positive logits: Nx1
         l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
         # negative logits: NxK
-        weight = (1 - 1 * loss_gan) ** (1.0 * self.count).cuda()
+        weight = t ** (1.0 * self.count).cuda()
         weight = torch.mul(self.queue, weight).cuda()
         l_neg = torch.einsum('nc,ck->nk', [q, weight.clone().detach()])
 
